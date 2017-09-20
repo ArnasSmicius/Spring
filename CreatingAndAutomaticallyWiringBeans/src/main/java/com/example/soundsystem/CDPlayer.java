082d3dbed913d495e89e73a00db2281599b5b6c1
ArnasSmicius <arnasmic@gmail.com>
2017-09-20 20:23:13 +0300

    Creating and automatically wiring beans

---
+++ CreatingAndAutomaticallyWiringBeans/src/main/java/com/example/soundsystem/CDPlayer.java
@@ -0,0 +1,12 @@
+package com.example.soundsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CDPlayer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CDPlayer.class);
+    }
+}
